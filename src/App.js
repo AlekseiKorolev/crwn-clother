3125e0ec74fc62cbab3f2c9ff3f510e83e3fbc56
AlekseiKorolev <42728999+AlekseiKorolev@users.noreply.github.com>
2020-05-11 11:46:10 +0200

    implemented firebgase utils, authenticated email password

--- src/App.js
+++ src/App.js
@@ -8,17 +8,26 @@ import ShopPage from "./pages/shop/shop.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 import Header from "./components/header/header.component";
 
-import { auth } from "./firebase/firebase.utils";
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 
 const App = () => {
   const [currentUser, setCurrentUser] = useState(null);
 
   useEffect(() => {
-    const unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      setCurrentUser(user);
-      console.log(user);
+    const unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot(snapShot => {
+          setCurrentUser({
+            id: snapShot.id,
+            ...snapShot.data()
+          });
+        });
+      }
+      setCurrentUser(userAuth);
     });
-    return () => unsubscribeFromAuth();
+    return () => unsubscribeFromAuth(); // ???
   }, []);
 
   return (
