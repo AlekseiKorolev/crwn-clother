0b0794b0c6e46d47938557095931de4a3bab78e1
AlekseiKorolev <42728999+AlekseiKorolev@users.noreply.github.com>
2020-05-30 15:41:21 +0200

    minor fix

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 import "./homepage.styles.scss";
 
-import { default as Directory } from "../../components/directory/directory.container";
+import Directory from "../../components/directory/directory.component";
 
 const HomePage = () => {
   return (
